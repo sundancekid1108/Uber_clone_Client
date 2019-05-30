b54486885f59ad29c8bce23240d1c8991314f079
Sundance <sundancekid1108@gmail.com>
2019-05-30 13:28:14 +0900

    프로젝트 초기화...
    프로젝트 초기화...

--- src/Routes/Login/index.tsx
+++
@@ -1,2 +0,0 @@
-import LoginPresenter from "./LoginPresenter";
-export default LoginPresenter;
--- src/sharedQueries.local.ts
+++
@@ -1,7 +0,0 @@
-import { gql } from "apollo-boost";
-
-export const LOG_USER_IN = gql`
-  mutation logUserIn($token: String!) {
-    logUserIn(token: $token) @client
-  }
-`;
